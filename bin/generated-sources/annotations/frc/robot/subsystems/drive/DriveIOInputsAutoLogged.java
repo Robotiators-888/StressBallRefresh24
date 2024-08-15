8accd099790b185cdb5857ea096f908bb4a54e12
AlexChapin <122164689+AlexChapin@users.noreply.github.com>
2024-08-15 09:49:12 -0400

    Added AdvantageKit Framework

---
+++ bin/generated-sources/annotations/frc/robot/subsystems/drive/DriveIOInputsAutoLogged.java
@@ -0,0 +1,48 @@
+package frc.robot.subsystems.drive;
+
+import java.lang.Cloneable;
+import java.lang.Override;
+import org.littletonrobotics.junction.LogTable;
+import org.littletonrobotics.junction.inputs.LoggableInputs;
+
+public class DriveIOInputsAutoLogged extends DriveIO.DriveIOInputs implements LoggableInputs, Cloneable {
+  @Override
+  public void toLog(LogTable table) {
+    table.put("LeftPositionRad", leftPositionRad);
+    table.put("LeftVelocityRadPerSec", leftVelocityRadPerSec);
+    table.put("LeftAppliedVolts", leftAppliedVolts);
+    table.put("LeftCurrentAmps", leftCurrentAmps);
+    table.put("RightPositionRad", rightPositionRad);
+    table.put("RightVelocityRadPerSec", rightVelocityRadPerSec);
+    table.put("RightAppliedVolts", rightAppliedVolts);
+    table.put("RightCurrentAmps", rightCurrentAmps);
+    table.put("GyroYaw", gyroYaw);
+  }
+
+  @Override
+  public void fromLog(LogTable table) {
+    leftPositionRad = table.get("LeftPositionRad", leftPositionRad);
+    leftVelocityRadPerSec = table.get("LeftVelocityRadPerSec", leftVelocityRadPerSec);
+    leftAppliedVolts = table.get("LeftAppliedVolts", leftAppliedVolts);
+    leftCurrentAmps = table.get("LeftCurrentAmps", leftCurrentAmps);
+    rightPositionRad = table.get("RightPositionRad", rightPositionRad);
+    rightVelocityRadPerSec = table.get("RightVelocityRadPerSec", rightVelocityRadPerSec);
+    rightAppliedVolts = table.get("RightAppliedVolts", rightAppliedVolts);
+    rightCurrentAmps = table.get("RightCurrentAmps", rightCurrentAmps);
+    gyroYaw = table.get("GyroYaw", gyroYaw);
+  }
+
+  public DriveIOInputsAutoLogged clone() {
+    DriveIOInputsAutoLogged copy = new DriveIOInputsAutoLogged();
+    copy.leftPositionRad = this.leftPositionRad;
+    copy.leftVelocityRadPerSec = this.leftVelocityRadPerSec;
+    copy.leftAppliedVolts = this.leftAppliedVolts;
+    copy.leftCurrentAmps = this.leftCurrentAmps.clone();
+    copy.rightPositionRad = this.rightPositionRad;
+    copy.rightVelocityRadPerSec = this.rightVelocityRadPerSec;
+    copy.rightAppliedVolts = this.rightAppliedVolts;
+    copy.rightCurrentAmps = this.rightCurrentAmps.clone();
+    copy.gyroYaw = this.gyroYaw;
+    return copy;
+  }
+}
---
+++ build/spotless/spotlessJava/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "StressBallRefresh24";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "45bd126d03e27f31beebf4bf894569ade5c4201b";
+  public static final String GIT_DATE = "2024-08-15 09:15:13 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-08-15 09:27:16 EDT";
+  public static final long BUILD_UNIX_TIME = 1723728436563L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
