b1fb7ca4950c452701537fc9bb018178dea25091
AlexChapin <122164689+AlexChapin@users.noreply.github.com>
2024-08-29 10:53:28 -0400

    Abandon AdvantageKit :(

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,15 +1,7 @@
-// Copyright 2021-2024 FRC 6328
-// http://github.com/Mechanical-Advantage
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// version 3 as published by the Free Software Foundation or
-// available in the root directory of this project.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
@@ -22,16 +14,44 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static final Mode currentMode = Mode.REAL;
 
-  public static enum Mode {
-    /** Running on a real robot. */
-    REAL,
+        //Drive Motors
+        public static final int ID_LEFT_PRIMARY = 22;  
+        public static final int ID_LEFT_SECONDARY = 23;  
+        public static final int ID_RIGHT_PRIMARY = 20;  
+        public static final int ID_RIGHT_SECONDARY = 21;  
 
-    /** Running a physics simulator. */
-    SIM,
+        //Speed for the motors
+        public static final Double speed = 0.6;
+        
 
-    /** Replaying from a log file. */
-    REPLAY
-  }
+        //Joystick
+        //this tells which port of the driver station the joystick is in
+        public static final int JOYSTICK_PORT = 0;  
+        
+        //This tells us which part of the joystick will be used
+        // 1 = to the left stick's y axis
+        public static final int LEFT_AXIS = 1; 
+        // 4 = to the right stick's y axis 
+        public static final int RIGHT_AXIS = 4;  
+        // 1 = the a button on joystick
+        public static final int A_BUTTON = 1;
+        // 2 = the b button on joystick
+        public static final int B_BUTTON = 2;
+        // 5 = the up on the d pad
+        public static final int DAPD_UP = 5;
+        // 6 = down on the d pad
+        public static final int DAPD_DOWN = 6;
+    
+
+        //Shooter
+        public static final int FLYWHEEL_LEFT = 0;
+        public static final int FLYWHEEL_RIGHT = 1;
+        public static final int ELEVATION = 2;
+        
+        public static final double FLYWHEEL_SPEED = .5;
+        public static final double ELEVATION_SPEED = 0.5;
+		public static final int RIGHT_TRIGGER = 3;
+    
 }
+
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,137 +1,86 @@
-// Copyright 2021-2024 FRC 6328
-// http://github.com/Mechanical-Advantage
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
-// version 3 as published by the Free Software Foundation or
-// available in the root directory of this project.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
 
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.auto.NamedCommands;
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.VideoMode;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import frc.robot.subsystems.drive.Drive;
-import frc.robot.subsystems.drive.DriveIO;
-import frc.robot.subsystems.drive.DriveIOSim;
-import frc.robot.subsystems.drive.DriveIOSparkMax;
-import frc.robot.subsystems.flywheel.Flywheel;
-import frc.robot.subsystems.flywheel.FlywheelIO;
-import frc.robot.subsystems.flywheel.FlywheelIOSim;
-import frc.robot.subsystems.flywheel.ShootIOSpark;
-import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
-import org.littletonrobotics.junction.networktables.LoggedDashboardNumber;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.tankDriveCMD;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.PneumaticsSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+import frc.robot.commands.ElevationDownCMD;
+import frc.robot.commands.ElevationUpCMD;
+import frc.robot.commands.FlywheelCMD;
+import frc.robot.commands.PistonCMD;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // Subsystems
-  private final Drive drive;
-  private final Flywheel flywheel;
+  // The robot's subsystems and commands are defined here...
+  private final DriveSubsystem driveSubsystem = new DriveSubsystem();
+  private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
+  private final PneumaticsSubsystem pneumaticsSubsystem = new PneumaticsSubsystem();
 
-  // Controller
-  private final CommandXboxController controller = new CommandXboxController(0);
+  // Creates joystick and joystick objects
+  public final Joystick joystick = new Joystick(Constants.JOYSTICK_PORT);
+  public final JoystickButton aButton = new JoystickButton(joystick, 1);
+  public final JoystickButton bButton = new JoystickButton(joystick, 2);
+  public final JoystickButton rBumper = new JoystickButton(joystick, 6);
+  public final JoystickButton lBumper = new JoystickButton(joystick, 5);
 
-  // Dashboard inputs
-  private final LoggedDashboardChooser<Command> autoChooser;
-  private final LoggedDashboardNumber flywheelSpeedInput =
-      new LoggedDashboardNumber("Flywheel Speed", 1500.0);
+  Trigger rightTrigger = new Trigger(() -> (joystick.getRawAxis(3) > 0.5));
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    switch (Constants.currentMode) {
-      case REAL:
-        // Real robot, instantiate hardware IO implementations
-        drive = new Drive(new DriveIOSparkMax());
-        flywheel = new Flywheel(new ShootIOSpark());
-        break;
-
-      case SIM:
-        // Sim robot, instantiate physics sim IO implementations
-        drive = new Drive(new DriveIOSim());
-        flywheel = new Flywheel(new FlywheelIOSim());
-        break;
-
-      default:
-        // Replayed robot, disable IO implementations
-        drive = new Drive(new DriveIO() {});
-        flywheel = new Flywheel(new FlywheelIO() {});
-        break;
-    }
-
-    // Set up auto routines
-    NamedCommands.registerCommand(
-        "Run Flywheel",
-        Commands.startEnd(
-                () -> flywheel.runVelocity(flywheelSpeedInput.get()), flywheel::stop, flywheel)
-            .withTimeout(5.0));
-
-
-    autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
-
-    // Set up SysId routines
-    autoChooser.addOption(
-        "Drive SysId (Quasistatic Forward)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Drive SysId (Quasistatic Reverse)",
-        drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    autoChooser.addOption(
-        "Drive SysId (Dynamic Forward)", drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-    autoChooser.addOption(
-        "Flywheel SysId (Quasistatic Forward)",
-        flywheel.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Flywheel SysId (Quasistatic Reverse)",
-        flywheel.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    autoChooser.addOption(
-        "Flywheel SysId (Dynamic Forward)", flywheel.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Flywheel SysId (Dynamic Reverse)", flywheel.sysIdDynamic(SysIdRoutine.Direction.kReverse));
-
-    // Configure the button bindings
     configureButtonBindings();
+    // CameraServer.startAutomaticCapture()
+    // .setVideoMode(new VideoMode(VideoMode.pixelFormat.kMJPEG, 416, 240, 60));
+    // // // This is creating a CMD that will be called and excuted as the robot is
+    // enabled we do this by making a defualt command
+    // This gets the requirements and the cmd construtor from eariler This gets the
+    // left stick so it controls the left motors This gets the right stick that
+    // controls the right motors this is the speed
+    driveSubsystem.setDefaultCommand(new tankDriveCMD(driveSubsystem, () -> -joystick.getRawAxis(Constants.RIGHT_AXIS),
+        () -> -joystick.getRawAxis(Constants.LEFT_AXIS), () -> Constants.speed));
+    // shooterSubsystem.setDefaultCommand(new FlywheelCMD(shooterSubsystem, () ->
+    // joystick.getRawAxis((Constants.RIGHT_TRIGGER))-0.5));
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
   private void configureButtonBindings() {
-    drive.setDefaultCommand(
-        Commands.run(
-            () -> drive.driveArcade(-controller.getLeftY(), controller.getLeftX()), drive));
-    controller
-        .a()
-        .whileTrue(
-            Commands.startEnd(
-                () -> flywheel.runVelocity(flywheelSpeedInput.get()), flywheel::stop, flywheel));
+
+    // Creates the A button to spin flywheels
+    // aButton.whileHeld(new FlywheelCMD(shooterSubsystem, () ->
+    // joystick.getRawButton(Constants.A_BUTTON)));
+
+    rightTrigger.whileTrue(new FlywheelCMD(shooterSubsystem));
+
+    // Creates the Left bumper to lift elevation up
+    lBumper.whileTrue((new ElevationUpCMD(shooterSubsystem, () -> joystick.getPOV(Constants.DAPD_UP))));
+
+    // Creates the Right bumper to lower elevation down
+    rBumper.whileTrue(new ElevationDownCMD(shooterSubsystem, () -> joystick.getPOV(Constants.DAPD_DOWN)));
+
+    // Creates B button to fire the piston when pressed
+    //bButton.toggleOnTrue();
+    bButton.onTrue(new PistonCMD(pneumaticsSubsystem));
+    
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
-    return autoChooser.get();
+    // An ExampleCommand will run in autonomous
+    return null;
   }
 }
