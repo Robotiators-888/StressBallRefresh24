28853e330a92d5b76c4f8172f26847ad059737fd
AlexChapin <122164689+AlexChapin@users.noreply.github.com>
2024-09-02 21:47:42 -0400

    Drivetrain Modernized

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,15 +5,11 @@
 
 package frc.robot;
 
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.VideoMode;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.commands.tankDriveCMD;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.PneumaticsSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
@@ -23,12 +19,9 @@ import frc.robot.commands.FlywheelCMD;
 import frc.robot.commands.PistonCMD;
 
 /**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
@@ -48,8 +41,13 @@ public class RobotContainer {
 
   public RobotContainer() {
     configureButtonBindings();
-  
-    driveSubsystem.setDefaultCommand(new RunCommand(driveSubsystem.setMotors(joystick.getRawAxis(Constants.RIGHT_AXIS),joystick.getRawAxis(Constants.LEFT_AXIS),Constants.DRIVE_SPEED),driveSubsystem));
+
+    driveSubsystem
+        .setDefaultCommand(
+            new RunCommand(
+                () -> driveSubsystem.setMotors(joystick.getRawAxis(Constants.RIGHT_AXIS),
+                    joystick.getRawAxis(Constants.LEFT_AXIS), Constants.DRIVE_SPEED),
+                driveSubsystem));
   }
 
   private void configureButtonBindings() {
@@ -57,14 +55,16 @@ public class RobotContainer {
     rightTrigger.whileTrue(new FlywheelCMD(shooterSubsystem));
 
     // Creates the Left bumper to lift elevation up
-    lBumper.whileTrue((new ElevationUpCMD(shooterSubsystem, () -> joystick.getPOV(Constants.DAPD_UP))));
+    lBumper.whileTrue(
+        (new ElevationUpCMD(shooterSubsystem, () -> joystick.getPOV(Constants.DAPD_UP))));
 
     // Creates the Right bumper to lower elevation down
-    rBumper.whileTrue(new ElevationDownCMD(shooterSubsystem, () -> joystick.getPOV(Constants.DAPD_DOWN)));
+    rBumper.whileTrue(
+        new ElevationDownCMD(shooterSubsystem, () -> joystick.getPOV(Constants.DAPD_DOWN)));
 
     // Creates B button to fire the piston when pressed
     bButton.onTrue(new PistonCMD(pneumaticsSubsystem));
-    
+
   }
 
   public Command getAutonomousCommand() {
