0b1069c41e5c72fb9518709033469f612a713e29
AlexChapin <122164689+AlexChapin@users.noreply.github.com>
2024-09-04 18:02:44 -0400

    Should Shoot and Do stuff

--- src/main/java/frc/robot/subsystems/IndexSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexSubsystem.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems;
 
 import frc.robot.Constants;
-import java.util.function.BooleanSupplier;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
